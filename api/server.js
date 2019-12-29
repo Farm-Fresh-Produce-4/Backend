03e012050052508a9225e173d6fbe6cd9fa9ce84
donutwizard666 <thebeardedwarrior@icloud.com>
2019-12-29 12:49:04 -0800

    basic structure of project complete

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const userRoute = require('../users/users-router.js');
+const farmerRoute = require('../farmers/farmer-router.js');
+const produceRoute = require('../produce/produce-router.js')
+const orderRoute = require('../orders/orders-router.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use('/api/auth/', userRoute, farmerRoute);
+server.use('/api/farmers', produceRoute);
