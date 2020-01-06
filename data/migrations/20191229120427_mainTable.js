fcb716b89f6ed906e01366c734ad9779bd78b6cc
donutwizard666 <thebeardedwarrior@icloud.com>
2020-01-06 14:38:21 -0800

    all necessary migrations built out. migrations batch run successful

--- data/migrations/20191229120427_mainTable.js
+++ data/migrations/20191229120427_mainTable.js
@@ -27,7 +27,7 @@ exports.up = function(knex) {
 
   .createTable('produce', tbl => {
     tbl
-      .integer('sku', 5)
+      .integer('produce_code', 5)
       .unique()
       .notNullable();
     tbl
@@ -35,6 +35,81 @@ exports.up = function(knex) {
       .unique()
       .notNullable();
     tbl.string('description');
+  })
+
+  .createTable('farmers_produce', tbl => {
+    tbl.integer('sku').unique();
+    tbl
+      .integer('farmer_id')
+      .references('id')
+      .inTable('farmers')
+      .notNullable()
+      .unsigned()
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    tbl
+      .integer('produce_code')
+      .references('produce_code')
+      .inTable('produce')
+      .notNullable();
+    tbl
+      .integer('quantity')
+      .unsigned()
+      .notNullable();
+    tbl.primary([ 'farmer_id', 'produce_code']);
+  })
+
+  .createTable('users_produce', tbl => {
+    tbl
+      .integer('user_id')
+      .references('id')
+      .inTable('users')
+      .notNullable()
+      .unsigned()
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    tbl
+      .integer('sku')
+      .references('sku')
+      .inTable('farmers_produce')
+      .notNullable()
+      .unsigned()
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    tbl
+      .integer('quantity')
+      .unsigned()
+      .notNullable();
+    tbl.primary([ 'user_id', 'sku' ]);
+  })
+
+  .createTable('orders', tbl => {
+    tbl
+      .string('id')
+      .notNullable()
+      .unique()
+      .primary();
+    tbl.string('address').notNullable();
+    tbl.date('order_date').notNullable();
+    tbl.boolean('delivered').notNullable();
+    tbl
+      .integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('RESTRICT');
+  })
+
+  .createTable('city', tbl => {
+    tbl.increments();
+    tbl.string('name').unique().notNullable();
+  })
+
+  .createTable('state', tbl => {
+    tbl.increments();
+    tbl.string('name').unique().notNullable();
   })
 
   .createTable('farms', tbl => {
@@ -69,56 +144,17 @@ exports.up = function(knex) {
       .onDelete('CASCADE');
   })
 
-  .createTable('city', tbl => {
-    tbl.increments();
-    tbl.string('name').unique().notNullable();
-  })
-
-  .createTable('state', tbl => {
-    tbl.increments();
-    tbl.string('name').unique().notNullable();
-  })
-
-  .createTable('produce', tbl => {
-    tbl.increments();
-    tbl.string('name').notNullable();
-    tbl.integer('quantity').notNullable();
-    tbl.float('price').notNullable();
-    tbl
-      .integer('farm_id')
-      .unsigned()
-      .notNullable()
-      .references('id')
-      .inTable('farms')
-      .onUpdate('CASCADE')
-      .onDelete('RESTRICT');
-      
-  })
-
-  .createTable('orders', tbl => {
-    tbl
-      .string('id')
-      .notNullable()
-      .unique()
-      .primary();
-    tbl.string('address').notNullable();
-    tbl.date('order_date').notNullable();
-    tbl.boolean('delivered').notNullable();
-    tbl
-      .integer('user_id')
-      .unsigned()
-      .notNullable()
-      .references('id')
-      .inTable('users')
-      .onUpdate('CASCADE')
-      .onDelete('RESTRICT');
-  })
-  
-
-
-
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('farmers');
+  return knex.schema
+    .dropTableIfExists('farms')
+    .dropTableIfExists('state')
+    .dropTableIfExists('city')
+    .dropTableIfExists('orders')
+    .dropTableIfExists('users_produce')
+    .dropTableIfExists('farmers_produce')
+    .dropTableIfExists('produce')
+    .dropTableIfExists('users')
+    .dropTableIfExists('farmers');
 };
