ae747c73f816c316cb5edabc57bc10c17fb72c18
donutwizard666 <thebeardedwarrior@icloud.com>
2020-01-09 20:22:41 -0800

    deleted unnecessary files

--- api/server.js
+++ api/server.js
@@ -8,9 +8,6 @@ const authenticate = require('../auth/authenticate-middleware.js');
 const authRoute = require('../auth/auth-router.js');
 const userRoute = require('../routes/users-router.js');
 const farmerRoute = require('../routes/farmer-router.js');
-const produceRoute = require('../routes/produce-router.js')
-const orderRoute = require('../routes/orders-router.js');
-
 
 const server = express();
 
@@ -19,7 +16,7 @@ server.use(cors());
 server.use(express.json());
 
 server.use('/api/auth', authRoute);
-server.use('/api/farmers', authenticate, produceRoute, orderRoute, farmerRoute);
+server.use('/api/farmers', authenticate, farmerRoute);
 server.use('/api/users', authenticate, userRoute);
 
 server.get('/', (req, res) => {
